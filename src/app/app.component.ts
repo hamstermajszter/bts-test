7a62c98e8e493b82f4d6ba790e7ea2f6190206d4
Viczán Balázs <hamstermajszter@gmail.com>
2020-06-17 23:25:11 +0200

    create overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import data from './data.json';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'overlay';
+  hidden = true;
+
+  animation: any = {};
+
+  prevRandom = 1;
+  data = data[this.prevRandom];
+
+
+  next() {
+    anime({
+      targets: '#overlay',
+      rotateY: '90deg',
+      duration: this.hidden ? 0 : 1000,
+      easing: 'easeInOutQuad'
+    })
+    setTimeout(() => {
+      let newRandom;
+      do {
+        newRandom = Math.floor(Math.random() * data.length);
+      } while (newRandom === this.prevRandom);
+      this.prevRandom = newRandom;
+      this.data = data[this.prevRandom];
+      anime({
+        targets: '#overlay',
+        rotateY: '0deg',
+        duration: 1000,
+        easing: 'easeInOutQuad'
+      });
+      this.hidden = false;
+    }, this.hidden ? 0 : 1000);
+  }
 }
